0473938f18a06405340d736798000f53feec0be3
CoralHeavens <48069528+CoralHeavens@users.noreply.github.com>
2023-04-30 03:44:31 +0300

    Top section

---
+++ src/components/layouts/TopSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TopSection() {
+    return (
+        <div className="w-screen flex justify-center pt-[100px] h-40">
+            <div className="text-white text-[74px] leading-[84px] tracking-wide text-center font-bold">
+                The best products<br/>start with Figma
+            </div>
+        </div>
+    )
+}
